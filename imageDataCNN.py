eb67773a8329326ec8b736a7319e46184df0e5cb
euhidaman <aman.derax20@gmail.com>
2022-02-23 21:18:49 +0530

    Created and trained the model

--- imageDataCNN.py
+++ imageDataCNN.py
@@ -92,3 +92,27 @@ plotImages(imgs)
 print(labels)
 
 # %%
+
+# Creating the model
+model = Sequential([
+    Conv2D(filters=32, kernel_size=(3, 3), activation='relu',
+           padding='same', input_shape=(224, 224, 3)),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Flatten(),
+    Dense(units=2, activation='softmax'),
+])
+
+model.summary()
+
+# %%
+
+# Compiling the model
+model.compile(optimizer=Adam(learning_rate=0.0001),
+              loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Training the model
+model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2)
+
+# %%
