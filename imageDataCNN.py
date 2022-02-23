872edb7a2fe434473cb48301fc26f84b74e9ac2a
euhidaman <aman.derax20@gmail.com>
2022-02-23 19:45:12 +0530

    Initial Commit

---
+++ imageDataCNN.py
@@ -0,0 +1,94 @@
+# %%
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+import itertools
+import os
+import shutil
+import random
+import glob
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# %%
+# This code is used to organize the data.
+# But, it's throwing a large dataset-error, so no need to run it
+os.chdir('data/dogs-vs-cats')
+if os.path.isdir('train/dog') is False:
+    os.makedirs('train/dog')
+    os.makedirs('train/cat')
+    os.makedirs('valid/dog')
+    os.makedirs('valid/cat')
+    os.makedirs('test/dog')
+    os.makedirs('test/cat')
+
+    for c in random.sample(glob.glob('cat*'), 500):
+        shutil.move(c, 'train/cat')
+    for c in random.sample(glob.glob('dog*'), 500):
+        shutil.move(c, 'train/dog')
+    for c in random.sample(glob.glob('cat*'), 100):
+        shutil.move(c, 'valid/cat')
+    for c in random.sample(glob.glob('dog*'), 100):
+        shutil.move(c, 'valid/dog')
+    for c in random.sample(glob.glob('cat*'), 50):
+        shutil.move(c, 'test/cat')
+    for c in random.sample(glob.glob('dog*'), 50):
+        shutil.move(c, 'test/dog')
+
+# %%
+os.chdir('../../')
+os.getcwd()
+
+# %%
+# Path to various datasets locations
+train_path = 'data/dogs-vs-cats/train'
+valid_path = 'data/dogs-vs-cats/valid'
+test_path = 'data/dogs-vs-cats/test'
+
+# %%
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=train_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10)
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=valid_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10)
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=test_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10, shuffle=False)
+
+# %%
+# Verifying correct no. of images were found in the correct place
+assert train_batches.n == 1000
+assert valid_batches.n == 200
+assert test_batches.n == 100
+assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
+
+# %%
+# Grabbing a single block of images and corresponding labels, from our training batch
+imgs, labels = next(train_batches)
+
+# %%
+
+# Function plots images in a grid of 1 row and 10 columns
+# Below code, is directly taken from Tensorflow's documentation
+
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+# %%
+plotImages(imgs)
+print(labels)
+
+# %%
